b15fbfbfc0fd993160f03fd5e60f2d76ced64b92
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-11 13:44:01 -0400

    Add src/main/java/com/kongrarainforest/KongraArmorMaterial.java

---
+++ src/main/java/com/kongrarainforest/KongraArmorMaterial.java
@@ -0,0 +1,55 @@
+package com.kongrarainforest;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+
+public class KongraArmorMaterial implements ArmorMaterial {
+    public static final KongraArmorMaterial INSTANCE = new KongraArmorMaterial();
+
+    private static final int[] DURABILITY = {13, 15, 16, 11};
+    private static final int[] PROTECTION = {4, 8, 7, 4};
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return DURABILITY[type.getEquipmentSlot().getEntitySlotId()] * 40;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return PROTECTION[type.getEquipmentSlot().getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(KongraRainforestModItems.KONGRA_ALLOY);
+    }
+
+    @Override
+    public String getName() {
+        return "kongra";
+    }
+
+    @Override
+    public float getToughness() {
+        return 3.0f;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.2f;
+    }
+}
